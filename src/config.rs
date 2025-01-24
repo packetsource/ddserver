a6500e7b5006425d956b7033351dd308902cf42a
Adam Chappell <Adam.Chappell@gtt.net>
2025-01-24 17:22:36 +0100

    initial commit

---
+++ src/config.rs
@@ -0,0 +1,78 @@
+use std::env;
+use std::process;
+use lazy_static::lazy_static;
+
+use crate::*;
+
+pub const DATA_DIR: &str = "/var/tmp";
+pub const BIND_ADDRESS: &str = "0.0.0.0:8001";
+
+#[derive(Debug)]
+pub struct Config {
+    pub verbose: bool,
+    pub data_dir: String,
+    pub bind_addr: String,
+    pub args: Vec<String>,
+}
+
+lazy_static! {
+
+    // Command line configuration
+    pub static ref CONFIG: Config = Config::cmdline();
+}
+
+// perhaps make a global structure of above so that it can
+// be referred to from anywhere in the namespace without an
+// instance variable?
+
+/* Chappell's lightweight getopt() for rust */
+impl Default for Config {
+    fn default() -> Config {
+        Config {
+            verbose: false,
+            data_dir: String::from(""),
+            bind_addr: String::from(BIND_ADDRESS),
+            args: vec![],
+        }
+    }
+}
+impl Config {
+    pub fn usage() {
+        eprintln!("Usage: ddserver [-v] [-d data-dir]");
+        process::exit(1);
+    }
+
+    pub fn cmdline() -> Config {
+        let mut config = Config::default();
+
+        let mut args = env::args();
+        args.next(); // blow off the first argument
+        while let Some(a) = args.next() {
+            config.args.push(match a.as_str() {
+                "-v" => {
+                    config.verbose = true;
+                    continue;
+                }
+                "-d" => {
+                    config.data_dir =
+                        args.next()
+                            .expect("expected data directory");
+                    std::env::set_current_dir(&config.data_dir)
+                        .expect("cannot use specified data directory");
+                    continue;
+                },
+                "-l" => {
+                    config.bind_addr =
+                        args.next()
+                            .expect("expected address to listen on, eg. 0.0.0.0:8001");
+                    continue;
+                }
+                _ => {
+                    a
+                }
+            })
+        }
+
+        config
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,60 @@
+#![allow(unused)]
+use std::net::SocketAddr;
+use tokio::net::{TcpListener, TcpStream};
+
+mod config; use config::CONFIG;
+
+#[tokio::main]
+pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+
+    loop {
+        let (mut socket, addr) = listener.accept().await?;
+        if CONFIG.verbose {
+            eprintln!("Connection from {}", &addr);
+        }
+
+        // Start a new task to handle the client
+        tokio::task::spawn(async move  {
+            match handle_client(&mut socket, &addr).await {
+                Ok(()) => {},
+                Err(e) => {
+                    eprintln!("{}: Error: {}", &addr, &e);
+                }
+            }
+            if CONFIG.verbose {
+                eprintln!("Connection from {} completed", &addr);
+            }
+        });
+    }
+
+    #[allow(unreachable_code)]
+    Ok(())
+}
+
+pub async fn handle_client(socket: &mut TcpStream, addr: &SocketAddr) -> Result<(), Box<dyn std::error::Error>> {
+
+    let filename: String = addr.ip().to_string();
+    if std::fs::exists(&filename)? {
+        // Serve the content back to the client, then unlink
+        let mut file = tokio::fs::OpenOptions::new()
+            .read(true)
+            .open(&filename)
+            .await?;
+        let bytes = tokio::io::copy(&mut file, socket).await?;
+        eprintln!("{} byte(s) to from file \"{}\" -> TCP client {}", bytes, &filename, &addr.ip());
+        tokio::fs::remove_file(&filename).await?;
+    } else {
+        // Store the content from the client
+        let mut file = tokio::fs::OpenOptions::new()
+            .write(true)
+            .create_new(true)
+            .open(&filename)
+            .await?;
+        let bytes = tokio::io::copy(socket, &mut file).await?;
+        eprintln!("{} byte(s) from TCP client {} -> file \"{}\"", bytes, &addr, &filename);
+    }
+
+    Ok(())
+}
